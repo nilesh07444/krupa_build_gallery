8f96f6647541f4d574ab297593ccfa62c8d4c252
krutikshahdev <krutik.shah1310@gmail.com>
2020-03-01 14:32:24 +0530

    Stock ViewModel added

---
+++ KrupaBuildGallery/ViewModel/ItemStockVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KrupaBuildGallery
+{
+    public class ItemStockVM
+    {
+        public long StockId { get; set; }        
+        [Required]
+        [Display(Name = "Category Name")]
+        public long CategoryId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public long ProductId { get; set; }
+        [Display(Name = "Sub Product Name")]
+        public long? SubProductId { get; set; }
+
+        [Required]
+        [Display(Name = "Product Item")]
+        public long ProductItemId { get; set; }
+      
+        [Required]
+        [Range(1,int.MaxValue, ErrorMessage = "Quatity must be greater than 0")]
+        [Display(Name = "Quantity")]
+        public long Quantity { get; set; }
+        public bool IsActive { get; set; }
+        
+        public List<SelectListItem> CategoryList { get; set; }
+        public List<SelectListItem> ProductList { get; set; }
+        public List<SelectListItem> SubProductList { get; set; }
+        public List<SelectListItem> ProductItemList { get; set; }
+
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public string SubProductName { get; set; }
+        public string ProductItemName { get; set; }
+    }
+
+
+}
