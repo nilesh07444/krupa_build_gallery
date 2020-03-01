5e5e205404e1755f86c2b7d53c11e27771c5e4d7
Nilesh Prajapati <prajapati.nileshbhai@gmail.com>
2020-03-01 09:51:48 +0530

    Product Item Add/List

--- KrupaBuildGallery/Areas/Admin/Controllers/ProductItemController.cs
+++ KrupaBuildGallery/Areas/Admin/Controllers/ProductItemController.cs
@@ -1,6 +1,10 @@
 ﻿using ConstructionDiary.Models;
+using KrupaBuildGallery.Helper;
+using KrupaBuildGallery.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Objects.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,18 +14,213 @@ namespace KrupaBuildGallery.Areas.Admin.Controllers
     [CustomAuthorize]
     public class ProductItemController : Controller
     {
-        // GET: Admin/ProductItem
+        private readonly krupagallarydbEntities _db;
+        public ProductItemController()
+        {
+            _db = new krupagallarydbEntities();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<ProductItemVM> lstProductItem = new List<ProductItemVM>();
+
+            try
+            {
+                lstProductItem = (from i in _db.tbl_ProductItems
+                                  join c in _db.tbl_Categories on i.CategoryId equals c.CategoryId
+                                  join p in _db.tbl_Products on i.ProductId equals p.Product_Id
+                                  join s in _db.tbl_SubProducts on i.SubProductId equals s.SubProductId into outerJoinSubProduct
+                                  from s in outerJoinSubProduct.DefaultIfEmpty()
+                                  where !i.IsDelete && !c.IsDelete && !p.IsDelete
+                                  select new ProductItemVM
+                                  {
+                                      ProductItemId = i.ProductItemId,
+                                      CategoryId = c.CategoryId,
+                                      ProductId = i.ProductId,
+                                      SubProductId = i.SubProductId,
+                                      ItemName = i.ItemName,
+                                      CategoryName = c.CategoryName,
+                                      ProductName = p.ProductName,
+                                      SubProductName = s.SubProductName,
+                                      MainImage = i.MainImage,
+                                      MRPPrice = i.MRPPrice,
+                                      CustomerPrice = i.CustomerPrice,
+                                      DistributorPrice = i.DistributorPrice,
+                                      IsActive = i.IsActive
+                                  }).OrderByDescending(x => x.ProductItemId).ToList();
+            }
+            catch (Exception ex)
+            {
+                string ErrorMessage = ex.Message.ToString();
+            }
+
+            return View(lstProductItem);
+
         }
+
         public ActionResult Add()
         {
-            return View();
+            ProductItemVM objProductItem = new ProductItemVM();
+
+            objProductItem.CategoryList = GetCategoryList();
+            objProductItem.ProductList = new List<SelectListItem>();
+            objProductItem.SubProductList = new List<SelectListItem>();
+
+            return View(objProductItem);
         }
-        public ActionResult Edit(int id)
+
+        [HttpPost]
+        public ActionResult Add(ProductItemVM productItemVM, HttpPostedFileBase ItemMainImageFile)
         {
-            return View();
+            try
+            {
+                IEnumerable<ModelError> allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                if (ModelState.IsValid)
+                {
+                    long LoggedInUserId = Int64.Parse(clsAdminSession.UserID.ToString());
+
+                    string fileName = string.Empty;
+                    string path = Server.MapPath("~/Images/ProductItemMedia/");
+                    if (ItemMainImageFile != null)
+                    {
+                        fileName = Guid.NewGuid() + "-" + Path.GetFileName(ItemMainImageFile.FileName);
+                        ItemMainImageFile.SaveAs(path + fileName);
+                    }
+                    else
+                    {
+                        fileName = productItemVM.MainImage;
+                    }
+
+                    tbl_ProductItems objProductItem = new tbl_ProductItems();
+                    objProductItem.CategoryId = productItemVM.CategoryId;
+                    objProductItem.ProductId = productItemVM.ProductId;
+                    objProductItem.SubProductId = productItemVM.SubProductId;
+                    objProductItem.ItemName = productItemVM.ItemName;
+                    objProductItem.ItemDescription = productItemVM.ItemDescription;
+                    objProductItem.Sku = productItemVM.Sku;
+                    objProductItem.MRPPrice = productItemVM.MRPPrice;
+                    objProductItem.CustomerPrice = productItemVM.CustomerPrice;
+                    objProductItem.DistributorPrice = productItemVM.DistributorPrice;
+                    objProductItem.GST_Per = productItemVM.GST_Per;
+                    objProductItem.IGST_Per = productItemVM.IGST_Per;
+                    objProductItem.Notification = productItemVM.Notification;
+                    objProductItem.MainImage = fileName;
+
+                    objProductItem.IsActive = true;
+                    objProductItem.IsDelete = false;
+                    objProductItem.CreatedBy = LoggedInUserId;
+                    objProductItem.CreatedDate = DateTime.UtcNow;
+                    objProductItem.UpdatedBy = LoggedInUserId;
+                    objProductItem.UpdatedDate = DateTime.UtcNow;
+
+                    _db.tbl_ProductItems.Add(objProductItem);
+                    _db.SaveChanges();
+
+                    return RedirectToAction("Index");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                string ErrorMessage = ex.Message.ToString();
+            }
+
+            return View(productItemVM);
         }
+
+        public ActionResult Edit(int Id)
+        {
+            ProductItemVM objProductItem = new ProductItemVM();
+
+            objProductItem = (from i in _db.tbl_ProductItems 
+                              where i.ProductItemId == Id
+                              select new ProductItemVM
+                              {
+                                  ProductItemId = i.ProductItemId,
+                                  CategoryId = i.CategoryId,
+                                  ProductId = i.ProductId,
+                                  SubProductId = i.SubProductId,
+                                  ItemName = i.ItemName, 
+                                  ItemDescription = i.ItemDescription,
+                                  MainImage = i.MainImage,
+                                  MRPPrice = i.MRPPrice,
+                                  CustomerPrice = i.CustomerPrice,
+                                  DistributorPrice = i.DistributorPrice,
+                                  GST_Per = i.GST_Per,
+                                  IGST_Per = i.IGST_Per,
+                                  Notification = i.Notification,
+                                  Sku = i.Sku,
+                                  IsActive = i.IsActive
+                              }).FirstOrDefault();
+
+            objProductItem.CategoryList = GetCategoryList();
+            objProductItem.ProductList = GetProductListByCategoryId(objProductItem.CategoryId);
+            objProductItem.SubProductList = GetSubProductListByProductId(objProductItem.ProductId);
+
+            return View(objProductItem);
+        }
+
+        [HttpPost]
+        public string DeleteProductItem(long ProductItemId)
+        {
+            string ReturnMessage = "";
+
+            try
+            {
+                tbl_ProductItems objProductItem = _db.tbl_ProductItems.Where(x => x.ProductItemId == ProductItemId && x.IsActive && !x.IsDelete).FirstOrDefault();
+
+                if (objProductItem == null)
+                {
+                    ReturnMessage = "notfound";
+                }
+                else
+                {
+                    long LoggedInUserId = Int64.Parse(clsAdminSession.UserID.ToString());
+
+                    objProductItem.IsDelete = true;
+                    objProductItem.UpdatedBy = LoggedInUserId;
+                    objProductItem.UpdatedDate = DateTime.UtcNow;
+
+                    _db.SaveChanges();
+                    ReturnMessage = "success";
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                ReturnMessage = "exception";
+            }
+
+            return ReturnMessage;
+        }
+         
+        private List<SelectListItem> GetCategoryList()
+        {
+            var CategoryList = _db.tbl_Categories.Where(x => x.IsActive && !x.IsDelete)
+                         .Select(o => new SelectListItem { Value = SqlFunctions.StringConvert((double)o.CategoryId).Trim(), Text = o.CategoryName })
+                         .OrderBy(x => x.Text).ToList();
+
+            return CategoryList;
+        }
+
+        private List<SelectListItem> GetProductListByCategoryId(long Id)
+        {
+            var ProductList = _db.tbl_Products.Where(x => x.IsActive && !x.IsDelete && x.CategoryId == Id)
+                         .Select(o => new SelectListItem { Value = SqlFunctions.StringConvert((double)o.Product_Id).Trim(), Text = o.ProductName })
+                         .OrderBy(x => x.Text).ToList();
+
+            return ProductList;
+        }
+
+        private List<SelectListItem> GetSubProductListByProductId(long Id)
+        {
+            var ProductList = _db.tbl_SubProducts.Where(x => x.IsActive && !x.IsDelete && x.ProductId == Id)
+                         .Select(o => new SelectListItem { Value = SqlFunctions.StringConvert((double)o.SubProductId).Trim(), Text = o.SubProductName })
+                         .OrderBy(x => x.Text).ToList();
+
+            return ProductList;
+        }
+
+
     }
 }
---
+++ KrupaBuildGallery/ViewModel/ProductItemVM.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KrupaBuildGallery
+{
+    public class ProductItemVM
+    {
+        public long ProductItemId { get; set; }
+        [Required]
+        [Display(Name = "Category Name")]
+        public long CategoryId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public long ProductId { get; set; }
+        [Display(Name = "Sub Product Name")]
+        public long? SubProductId { get; set; }
+        [Required]
+        [Display(Name = "Item Name")]
+        public string ItemName { get; set; }
+        [Required]
+        [Display(Name = "Item Description")]
+        public string ItemDescription { get; set; }
+        [Required]
+        [Display(Name = "MRP Price")]
+        public decimal MRPPrice { get; set; }
+        [Required]
+        [Display(Name = "Customer Price")]
+        public decimal CustomerPrice { get; set; }
+        [Required]
+        [Display(Name = "Distributor Price")]
+        public decimal DistributorPrice { get; set; }
+        [Required]
+        [Display(Name = "GST Per")]
+        public decimal GST_Per { get; set; }
+        [Display(Name = "IGST Per")]
+        public decimal IGST_Per { get; set; } 
+        public string MainImage { get; set; }
+        [Display(Name = "Notification Text")]
+        public string Notification { get; set; }
+        [Display(Name = "Sku")]
+        public string Sku { get; set; }
+        public bool IsActive { get; set; }
+        [Display(Name = "Item Main Image")]
+        public HttpPostedFileBase ItemMainImageFile { get; set; }
+
+        public List<SelectListItem> CategoryList { get; set; }
+        public List<SelectListItem> ProductList { get; set; }
+        public List<SelectListItem> SubProductList { get; set; }
+
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public string SubProductName { get; set; }
+
+    }
+}
