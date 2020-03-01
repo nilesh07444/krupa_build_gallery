53a2bc13b4e6fd260dc564f349e5a956a11faa7f
Nilesh Prajapati <prajapati.nileshbhai@gmail.com>
2020-03-01 17:09:22 +0530

    Sidebar bind dynamically

---
+++ KrupaBuildGallery/ViewModel/CategoryInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KrupaBuildGallery
+{
+     
+    public class CategoryInfo
+    {
+        public long CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryImage { get; set; }
+        public IEnumerable<ProductInfo> ProductInfo { get; set; }
+    }
+
+    public class ProductInfo
+    {
+        public long ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImage { get; set; }
+        public IEnumerable<SubProductInfo> SubProductInfo { get; set; }
+    }
+
+    public class SubProductInfo
+    {
+        public long SubProductId { get; set; }
+        public string SubProductName { get; set; }
+        public string SubProductImage { get; set; }
+    }
+
+}
