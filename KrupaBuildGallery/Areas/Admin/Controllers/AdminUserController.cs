210506e06b1f5e806b0a62fd12e19bb5522cae80
Nilesh Prajapati <prajapati.nileshbhai@gmail.com>
2020-03-15 08:58:05 +0530

    - admin panel change password feature added

--- KrupaBuildGallery/Areas/Admin/Controllers/AdminUserController.cs
+++ KrupaBuildGallery/Areas/Admin/Controllers/AdminUserController.cs
@@ -1,4 +1,5 @@
 ﻿using ConstructionDiary.Models;
+using KrupaBuildGallery.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,11 @@ namespace KrupaBuildGallery.Areas.Admin.Controllers
     [CustomAuthorize]
     public class AdminUserController : Controller
     {
-        // GET: Admin/AdminUser
+        private readonly krupagallarydbEntities _db;
+        public AdminUserController()
+        {
+            _db = new krupagallarydbEntities();
+        }
         public ActionResult Index()
         {
             return View();
@@ -19,5 +24,43 @@ namespace KrupaBuildGallery.Areas.Admin.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public string ChangePassword(FormCollection frm)
+        {
+            string ReturnMessage = "";
+            try
+            {
+                string CurrentPassword = frm["currentpwd"];
+                string NewPassword = frm["newpwd"];
+
+                long LoggedInUserId = Int64.Parse(clsAdminSession.UserID.ToString());
+                tbl_AdminUsers objUser = _db.tbl_AdminUsers.Where(x => x.AdminUserId == LoggedInUserId).FirstOrDefault();
+
+                if (objUser != null)
+                {
+                    string EncryptedCurrentPassword = CurrentPassword; // CoreHelper.Encrypt(CurrentPassword);
+                    if (objUser.Password == EncryptedCurrentPassword)
+                    {
+                        objUser.Password = NewPassword; //CoreHelper.Encrypt(NewPassword);
+                        _db.SaveChanges();
+
+                        ReturnMessage = "SUCCESS";
+                        Session.Clear();
+                    }
+                    else
+                    {
+                        ReturnMessage = "CP_NOT_MATCHED";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ReturnMessage = "ERROR";
+            }
+
+            return ReturnMessage;
+        }
+         
     }
 }
