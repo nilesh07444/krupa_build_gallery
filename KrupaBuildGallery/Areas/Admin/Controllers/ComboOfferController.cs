df61aaa904b97ee999fa1c3e134983d3a7824392
Nilesh Prajapati <prajapati.nileshbhai@gmail.com>
2020-06-03 17:58:19 +0530

    Design Combo offer

---
+++ KrupaBuildGallery/Areas/Admin/Controllers/ComboOfferController.cs
@@ -0,0 +1,170 @@
+﻿using ConstructionDiary.Models;
+using KrupaBuildGallery.Helper;
+using KrupaBuildGallery.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Objects.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KrupaBuildGallery.Areas.Admin.Controllers
+{
+    public class ComboOfferController : Controller
+    {
+        private readonly krupagallarydbEntities _db;
+
+        public ComboOfferController()
+        {
+            _db = new krupagallarydbEntities();
+        }
+        public ActionResult Index()
+        {
+            List<ComboOfferVM> lstOfferVm = new List<ComboOfferVM>();
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return View(lstOfferVm);
+        }
+
+        public ActionResult Add()
+        {
+            ComboOfferVM objOffer = new ComboOfferVM();
+
+            objOffer.Main_CategoryList = GetCategoryList();
+            objOffer.Main_ProductList = new List<SelectListItem>();
+            objOffer.Main_SubProductList = new List<SelectListItem>();
+            objOffer.Main_ProductItemList = new List<SelectListItem>();
+
+            objOffer.Sub_CategoryList = GetCategoryList();
+            objOffer.Sub_ProductList = new List<SelectListItem>();
+            objOffer.Sub_SubProductList = new List<SelectListItem>();
+            objOffer.Sub_ProductItemList = new List<SelectListItem>();
+
+            return View(objOffer);
+        }
+
+        [HttpPost]
+        public ActionResult Add(ComboOfferVM objOffer)
+        {
+            return View(objOffer);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            ComboOfferVM objOffer = new ComboOfferVM();
+
+            try
+            {
+
+
+                objOffer.Main_CategoryList = GetCategoryList();
+                objOffer.Main_ProductList = new List<SelectListItem>();
+                objOffer.Main_SubProductList = new List<SelectListItem>();
+                objOffer.Main_ProductItemList = new List<SelectListItem>();
+
+                objOffer.Sub_CategoryList = GetCategoryList();
+                objOffer.Sub_ProductList = new List<SelectListItem>();
+                objOffer.Sub_SubProductList = new List<SelectListItem>();
+                objOffer.Sub_ProductItemList = new List<SelectListItem>();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return View(objOffer);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ComboOfferVM objOffer)
+        {
+            return View(objOffer);
+        }
+
+        private List<SelectListItem> GetCategoryList()
+        {
+            var CategoryList = _db.tbl_Categories.Where(x => x.IsActive && !x.IsDelete)
+                         .Select(o => new SelectListItem { Value = SqlFunctions.StringConvert((double)o.CategoryId).Trim(), Text = o.CategoryName })
+                         .OrderBy(x => x.Text).ToList();
+
+            return CategoryList;
+        }
+
+        [HttpPost]
+        public string DeleteOffer(long ComboOfferId)
+        {
+            string ReturnMessage = "";
+
+            try
+            {
+                tbl_Offers objtbloffer = _db.tbl_Offers.Where(x => x.OfferId == ComboOfferId && x.IsActive && !x.IsDelete).FirstOrDefault();
+
+                if (objtbloffer == null)
+                {
+                    ReturnMessage = "notfound";
+                }
+                else
+                {
+                    long LoggedInUserId = Int64.Parse(clsAdminSession.UserID.ToString());
+
+                    objtbloffer.IsDelete = true;
+                    objtbloffer.UpdatedBy = LoggedInUserId;
+                    objtbloffer.UpdatedDate = DateTime.UtcNow;
+
+                    _db.SaveChanges();
+                    ReturnMessage = "success";
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                ReturnMessage = "exception";
+            }
+
+            return ReturnMessage;
+        }
+
+        [HttpPost]
+        public string ChangeStatus(long Id, string Status)
+        {
+            string ReturnMessage = "";
+            try
+            {
+                tbl_Offers objtbl_Offers = _db.tbl_Offers.Where(x => x.OfferId == Id).FirstOrDefault();
+
+                if (objtbl_Offers != null)
+                {
+                    long LoggedInUserId = Int64.Parse(clsAdminSession.UserID.ToString());
+                    if (Status == "Active")
+                    {
+                        objtbl_Offers.IsActive = true;
+                    }
+                    else
+                    {
+                        objtbl_Offers.IsActive = false;
+                    }
+
+                    objtbl_Offers.UpdatedBy = LoggedInUserId;
+                    objtbl_Offers.UpdatedDate = DateTime.UtcNow;
+
+                    _db.SaveChanges();
+                    ReturnMessage = "success";
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                ReturnMessage = "exception";
+            }
+
+            return ReturnMessage;
+        }
+
+    }
+}
---
+++ KrupaBuildGallery/ViewModel/Admin/ComboOfferVM.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KrupaBuildGallery
+{
+    public class ComboOfferVM
+    {
+        public long ComboOfferId { get; set; }
+        [Required, Display(Name = "Offer Title")] 
+        public string OfferTitle { get; set; } 
+        [Required(ErrorMessage = "Required"), Display(Name = "Category")]
+        public long Main_CategoryId { get; set; } 
+
+        [Required(ErrorMessage = "Required"), Display(Name = "Product")]
+        public long Main_ProductId { get; set; }
+
+        [Display(Name = "Sub Product")]
+        public long? Main_SubProductId { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Required")]
+        [Display(Name = "Product Item")]
+        public long Main_ProductItemId { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Required")]
+        [Display(Name = "Qty")]
+        public decimal Main_Qty { get; set; }
+
+        // Sub Product Fields
+        [Required(ErrorMessage = "Required"), Display(Name = "Category")]
+        public long Sub_CategoryId { get; set; } 
+
+        [Required(ErrorMessage = "Required"), Display(Name = "Product")]
+        public long Sub_ProductId { get; set; }
+
+        [Display(Name = "Sub Product")]
+        public long? Sub_SubProductId { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Required")]
+        [Display(Name = "Product Item")]
+        public long Sub_ProductItemId { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Required")]
+        [Display(Name = "Qty")]
+        public decimal Sub_Qty { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Offer Price must be greater than 0")]
+        [Display(Name = "Combo Offer Price")]
+        public decimal ComboOfferPrice { get; set; }
+        
+        [Required,Display(Name = "Offer Start Date")] 
+        public string OfferStartDate { get; set; }
+
+        [Required, Display(Name = "Offer End Date")] 
+        public string OfferEndDate { get; set; }
+        [Display(Name = "Offer Image")]
+        public HttpPostedFileBase OfferImageFile { get; set; }
+        public bool IsActive { get; set; }
+
+        // Additional Fields
+        // main fields
+        public List<SelectListItem> Main_CategoryList { get; set; }
+        public List<SelectListItem> Main_ProductList { get; set; }
+        public List<SelectListItem> Main_SubProductList { get; set; }
+        public List<SelectListItem> Main_ProductItemList { get; set; }
+        public string Main_CategoryName { get; set; }
+        public string Main_ProductName { get; set; }
+        public string Main_SubProductName { get; set; }
+        public string Main_ProductItemName { get; set; }
+
+        // sub fields
+        public List<SelectListItem> Sub_CategoryList { get; set; }
+        public List<SelectListItem> Sub_ProductList { get; set; }
+        public List<SelectListItem> Sub_SubProductList { get; set; }
+        public List<SelectListItem> Sub_ProductItemList { get; set; }
+        public string Sub_CategoryName { get; set; }
+        public string Sub_ProductName { get; set; }
+        public string Sub_SubProductName { get; set; }
+        public string Sub_ProductItemName { get; set; }
+
+        public DateTime? dtOfferStartDate { get; set; }
+        public DateTime? dtOfferEndDate { get; set; }
+        public string OfferImage { get; set; }
+    }
+}
