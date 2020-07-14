dab243cf6bfffc3d641590c203664aa82294b63e
Krutik Shah <krutik9033331394@outlook.com>
2020-07-14 21:45:31 +0530

    ccc

---
+++ KrupaBuildGallery/ViewModel/ReportVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KrupaBuildGallery.ViewModel
+{
+    public class ReportVM
+    {
+        public string Date { get; set; }
+        public string Opening { get; set; }
+        public string Credit { get; set; }
+        public string Debit { get; set; }
+        public string Closing { get; set; }
+        public string Remarks { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
