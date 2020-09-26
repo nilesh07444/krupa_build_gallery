0bb7450418b04e1f9f07992f0bf59363ab11c6ad
Nilesh Prajapati <prajapati.nileshbhai@gmail.com>
2020-09-26 19:59:49 +0530

    feedback list

--- KrupaBuildGallery/ViewModel/FeedbackVM.cs
+++ KrupaBuildGallery/ViewModel/FeedbackVM.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace KrupaBuildGallery.ViewModel
+namespace KrupaBuildGallery
 {
     public class FeedbackVM
     {
@@ -13,8 +13,13 @@ namespace KrupaBuildGallery.ViewModel
         public string AboutDeliveryBoyBehaviour { get; set; }
         public string OurQuality { get; set; }
         public string Suggestion { get; set; }
-        public DateTime FeedbackDate { get; set; }
-        public long ClientUserId { get; set; }
-        public DateTime FeedbackOfMonth { get; set; }
+        public DateTime? FeedbackDate { get; set; }
+        public long? ClientUserId { get; set; }
+        public DateTime? FeedbackOfMonth { get; set; }
+
+        //
+        public string UserName { get; set; }
+        public long ClientRoleId { get; set; }
+        public string MobileNo { get; set; }
     }
 }
