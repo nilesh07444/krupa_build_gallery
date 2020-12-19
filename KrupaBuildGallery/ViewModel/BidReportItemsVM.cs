f93ca964b9a18cfe36230b67f295db6e1fa5bbf2
Krutik Shah <krutik9033331394@outlook.com>
2020-12-19 18:54:18 +0530

    Commit

---
+++ KrupaBuildGallery/ViewModel/BidReportItemsVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KrupaBuildGallery
+{
+    public class BidReportItemsVM
+    {
+        public long ItemId { get; set; }
+        public string ItemName { get; set; }
+        public List<BidVM> lstBids { get; set; }
+    }
+}
